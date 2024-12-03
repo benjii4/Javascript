c5ae3a1d97460c371421830e39dca3b105227ee6
benjii4 <benjaminr.corcur@educa.jcyl.es>
2024-12-03 11:40:57 +0100

    Benjamin

---
+++ Actividad2.js
@@ -0,0 +1,16 @@
+function Suma (num1, num2){
+    var n;
+    n = num1+num2;
+    alert(n)
+
+    return n;
+}
+function Resta (num1, num2){
+    var n;
+    n = num1-num2;
+    alert(n)
+
+    return n;
+}
+Suma(10, 2)
+Resta(12, 7)
